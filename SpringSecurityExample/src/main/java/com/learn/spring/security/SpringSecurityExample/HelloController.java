9b5c4502d60c626d500fa10a4004d7f708bbf535
Pranab Pal <pranabpal349@gmail.com>
2026-07-28 01:48:58 +0530

    CSRF implentaion, disabling csrf and working with session

---
+++ SpringSecurityExample/src/main/java/com/learn/spring/security/SpringSecurityExample/HelloController.java
@@ -0,0 +1,13 @@
+package com.learn.spring.security.SpringSecurityExample;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet(HttpServletRequest request) {
+        return "Welcome to Spring Security" + request.getSession().getId();
+    }
+}

0077621cddcc9c97e601006d971dae6a05a37630
Pranab Pal <pranabpal349@gmail.com>
2026-09-08 00:42:21 +0530

    folder name changed

--- SpringSecurity_JWT_Implementation/src/main/java/com/learn/spring/security/SpringSecurityExample/controller/HelloController.java
+++ SpringSecurity_JWT_Implementation/src/main/java/com/learn/spring/security/SpringSecurityExample/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String about() {
         return "Hello World";
     }

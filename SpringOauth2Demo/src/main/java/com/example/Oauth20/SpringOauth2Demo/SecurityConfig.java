9e2676c24e11b807df03af5b370c1da3aef655bf
Pranab Pal <pranabpal349@gmail.com>
2026-09-24 02:01:22 +0530

    Implementation Oauth2.0 in springsecurity

---
+++ SpringOauth2Demo/src/main/java/com/example/Oauth20/SpringOauth2Demo/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.Oauth20.SpringOauth2Demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Configure security settings here
+        http.authorizeHttpRequests(auth -> auth// Allow access to the root endpoint without authentication
+                        .anyRequest().authenticated())// Require authentication for any other request
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+}
